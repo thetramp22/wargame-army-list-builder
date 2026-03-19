80083e0759e5c4f92d92cfb7d2b21c927da1f957
Scott Starks <esstarks@hotmail.com>
2026-03-19 15:46:23 -0600

    added do_create function

--- cli/interface.py
+++ cli/interface.py
@@ -1,15 +1,40 @@
 import cmd
+from loaders.game_data import GameData
+from services.army_builder import ArmyBuilder
+from services.army_repository import ArmyRepository
 
 class Interface(cmd.Cmd):
   prompt = "ArmyBldr>> "
   intro = "ArmyBuilderCLI, Type 'help' for available commands."
 
-  def __init__(self):
+  def __init__(self, game_data: GameData, army_builder: ArmyBuilder, army_repository: ArmyRepository):
     super().__init__()
+    self.game_data = game_data
+    self.army_builder = army_builder
+    self.army_repository = army_repository
+    self.current_army = None
+    # for future support of multiple games, currently only 1 game supported
+    self.current_game_id = "warhammer_40k"
 
-  def do_create_army(self, line):
-    """Create a new army"""
-    pass
+  def do_create(self, line):
+    """Create a new army
+
+    Usage: create <name> <faction_id>
+    Creates a new army with the given name and faction.
+    Example: create my_army space_marines 
+    """
+    arguments = line.split()
+    if len(arguments) != 2:
+      print("*** invalid number of arguments")
+      return
+    name, faction_id = arguments
+    result = self.army_builder.create_army(name, self.current_game_id, faction_id)
+    if not result["success"]:
+      for message in result["messages"]:
+        print(f"*** {message}")
+      return
+    self.current_army = result["army"]
+    print(f"Army '{name}' created (Faction: {self.current_army.faction.name})")
 
   def do_test(self, line):
     """Test command"""
--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from loaders.data_loader import DataLoader
 from services.army_builder import ArmyBuilder
 from cli.interface import Interface
 
-filepath = "./data/armies.json"
-
 interface = Interface()
 
 def main():
   loader = DataLoader()
   game_data = loader.load()
-  army_builder = ArmyBuilder(game_data, filepath)
   
 
 # if __name__ == '__main__':
