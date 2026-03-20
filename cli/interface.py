36e0d6bf935e9d9f8b78dee658cfa70496509d7e
Scott Starks <esstarks@hotmail.com>
2026-03-20 10:17:04 -0600

    updated do_create to take new input from army_builder

--- cli/interface.py
+++ cli/interface.py
@@ -3,47 +3,74 @@ from loaders.game_data import GameData
 from services.army_builder import ArmyBuilder
 from services.army_repository import ArmyRepository
 
+
 class Interface(cmd.Cmd):
-  prompt = "ArmyBldr>> "
-  intro = "ArmyBuilderCLI, Type 'help' for available commands."
+    prompt = "ArmyBldr>> "
+    intro = "ArmyBuilderCLI, Type 'help' for available commands."
 
-  def __init__(self, game_data: GameData, army_builder: ArmyBuilder, army_repository: ArmyRepository):
-    super().__init__()
-    self.game_data = game_data
-    self.army_builder = army_builder
-    self.army_repository = army_repository
-    self.current_army = None
-    # for future support of multiple games, currently only 1 game supported
-    self.current_game_id = "warhammer_40k"
+    def __init__(
+        self,
+        game_data: GameData,
+        army_builder: ArmyBuilder,
+        army_repository: ArmyRepository,
+    ):
+        super().__init__()
+        self.game_data = game_data
+        self.army_builder = army_builder
+        self.army_repository = army_repository
+        self.current_army = None
+        # for future support of multiple games, currently only 1 game supported
+        self.current_game_id = "warhammer_40k"
 
-  def do_create(self, line):
-    """Create a new army
+    def do_create(self, line):
+        """Create a new army
 
-    Usage: create <name> <faction_id>
-    Creates a new army with the given name and faction.
-    Example: create my_army space_marines 
-    """
-    arguments = line.split()
-    if len(arguments) != 2:
-      print("*** invalid number of arguments")
-      return
-    name, faction_id = arguments
-    result = self.army_builder.create_army(name, self.current_game_id, faction_id)
-    if not result["success"]:
-      for message in result["messages"]:
-        print(f"*** {message}")
-      return
-    self.current_army = result["army"]
-    print(f"Army '{name}' created (Faction: {self.current_army.faction.name})")
+        Usage: create <name> <faction_id>
+        Creates a new army with the given name and faction.
+        Example: create my_army space_marines
+        """
+        arguments = line.split()
+        if len(arguments) != 2:
+            print("*** invalid number of arguments")
+            return
+        name, faction_id = arguments
+        result = self.army_builder.create_army(name, self.current_game_id, faction_id)
+        if not result["success"]:
+            for message in result["messages"]:
+                print(f"*** {message}")
+            return
+        self.current_army = result["army"]
+        print(f"Army '{name}' created (Faction: {self.current_army.faction.name})")
 
-  def do_test(self, line):
-    """Test command"""
-    print("Test command output")
+    def do_add(self, line):
+        """Add unit to army"""
 
-  def do_quit(self, line):
-    """Exit the CLI"""
-    return True
-  
-  def postcmd(self, stop, line):
-    print()  # Add an empty line for better readability
-    return stop
+    def do_show(self, line):
+        """Show summary of current army"""
+
+    def do_save(self, line):
+        """Save current army to file"""
+
+    def do_load(self, line):
+        """Load army from file"""
+
+    def do_list(self, line):
+        """List armies from file"""
+
+    def do_delete(self, line):
+        """Delete army from file"""
+
+    def do_validate(self, line):
+        """Check that current army is valid for play"""
+
+    def do_test(self, line):
+        """Test command"""
+        print("Test command output")
+
+    def do_quit(self, line):
+        """Exit the CLI"""
+        return True
+
+    def postcmd(self, stop, line):
+        print()  # Add an empty line for better readability
+        return stop
